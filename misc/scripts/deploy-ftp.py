45122042711d407adf583d5526c459a052095af7
Somebody Whoisbored <13044396+shadowninja108@users.noreply.github.com>
2023-04-07 11:32:48 -0700

    Update deploy-ftp.py

--- misc/scripts/deploy-ftp.py
+++ misc/scripts/deploy-ftp.py
@@ -8,44 +8,49 @@ from typing import Any, Dict, Optional, Self, Tuple, Union
 # https://ftputil.sschwarzer.net
 import ftputil
 
-
+# Wrapper to assert the enviroment variable exists.
 def getenv(key: str, default: Optional[str] = None, *args: Tuple[Any], **kwargs: Dict[str, Any]) -> str:
     '''Wrapper for os.getenv.'''
     
     value: Union[None, str] = os.getenv(key)
+    # Use the value if it exists.
     if value is not None:
         return value
+    # If we're provided a default, fallback to that.
     elif default is not None:
         return default
+    # Assert key is missing.
     else:
-        raise KeyError
+        raise KeyError(f'Missing enviroment variable {key}! Only run this script via make deploy-ftp.')
 
 
 PROGRAM_ID: str = getenv('PROGRAM_ID')
 
+# Get enviroment variables from build system.
 FTP_IP: str = getenv('FTP_IP')
 FTP_PORT: int = int(getenv('FTP_PORT'))
 FTP_USERNAME: str = getenv('FTP_USERNAME')
-FTP_PASSWORD: str = getenv('FTP_PASSWORD', '')
-
 OUT: str = getenv('OUT')
 SD_OUT: str = getenv('SD_OUT')
+# Password may be empty, so special case this.
+FTP_PASSWORD: str = getenv('FTP_PASSWORD', '')
 
 
 class SessionFactory(ftplib.FTP):
     '''Session factory for FTPHost.'''
     
-    def __init__(self: Self, ftp_ip: str = '192.168.0.235', ftp_port: int = 5000, ftp_username: str = 'anonymous',
-                 ftp_password: str = '', *args: Tuple[Any], **kwargs: Dict[str, Any]) -> NoneType:
+    def __init__(self: Self, ftp_ip: str, ftp_port: int, ftp_username: str, ftp_password: str, *args: Tuple[Any], **kwargs: Dict[str, Any]) -> NoneType:
         super().__init__()
         
         try:
-            self.connect(ftp_ip, ftp_port)  # connect to ftp_ip and ftp_port
+            # Connect to FTP server.
+            self.connect(ftp_ip, ftp_port)
         except ftputil.error.FTPOSError:
             raise
         else:
             try:
-                self.login(ftp_username, ftp_password)  # login as ftp_username (ftp_password is optional)
+                # Login with credentials.
+                self.login(ftp_username, ftp_password)
             except ftputil.error.FTPOSError:
                 raise
             else:
@@ -57,10 +62,19 @@ class SessionFactory(ftplib.FTP):
 
 
 def main(*args: Tuple[Any], **kwargs: Dict[str, Any]) -> NoneType:
+    # Connect/login to console FTP server.
     with ftputil.FTPHost(FTP_IP, FTP_PORT, FTP_USERNAME, FTP_PASSWORD, session_factory=SessionFactory) as ftp_host:
-        ftp_host.makedirs(SD_OUT, exist_ok=True)  # make SD_OUT
-        ftp_host.upload(os.path.join(OUT, 'main.npdm'), ftp_host.path.join(SD_OUT, 'main.npdm'))  # upload main.npdm
-        ftp_host.upload(os.path.join(OUT, 'subsdk9'), ftp_host.path.join(SD_OUT, 'subsdk9'))  # upload subsdk9
+        # Make output directory.
+        ftp_host.makedirs(SD_OUT, exist_ok=True)
+        
+        # Iterate every file in the deploy directory.
+        for name in os.listdir(OUT):
+            # Ignore directories, for now.
+            if not os.path.isfile(os.path.join(OUT, name)):
+                continue
+                
+            # Upload file to server.
+            ftp_host.upload(os.path.join(OUT, name), ftp_host.path.join(SD_OUT, name))
 
 
 if __name__ == '__main__':
