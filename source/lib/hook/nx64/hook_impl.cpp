3bb46d0e346da92b70ea558b394ee392439b0ff8
Somebody Whoisbored <13044396+shadowninja108@users.noreply.github.com>
2023-04-14 18:12:53 -0700

    Fix to account for devkitPro changes

--- source/lib/hook/nx64/hook_impl.cpp
+++ source/lib/hook/nx64/hook_impl.cpp
@@ -42,8 +42,6 @@
 #define __page_size PAGE_SIZE
 #define __page_align(n) __align_up(static_cast<uintptr_t>(n), __page_size)
 #define __ptr_align(x) __ptr(__align_down(reinterpret_cast<uintptr_t>(x), __page_size))
-#define __align_up(x, n) (((x) + ((n)-1)) & ~((n)-1))
-#define __align_down(x, n) ((x) & -(n))
 #define __countof(x) static_cast<intptr_t>(sizeof(x) / sizeof((x)[0]))  // must be signed
 #define __atomic_increase(p) __sync_add_and_fetch(p, 1)
 #define __sync_cmpswap(p, v, n) __sync_bool_compare_and_swap(p, v, n)
@@ -62,19 +60,6 @@ namespace exl::hook::nx64 {
 
         typedef uint32_t HookPool[HookMax][TrampolineSize];
         static_assert(sizeof(HookPool) <= HookPoolSize, "");
-
-        // Inline hooking constants
-        extern const u64 InlineHandlerStart;
-        extern const u64 InlineHandlerEnd;
-        constexpr size_t InlineHookHandlerSize = 0x9C; // correct if handler size changes
-        struct PACKED InlineHookEntry {
-            char m_Handler[InlineHookHandlerSize];
-            void* m_Callback;
-            void* m_Trampoline;
-        };
-        constexpr size_t InlineHookSize = sizeof(InlineHookEntry);
-        constexpr size_t InlineHookMax = 1;
-        constexpr size_t InlineHookPoolSize = InlineHookSize * InlineHookMax;
 
         typedef uint32_t* __restrict* __restrict instruction;
         typedef struct {
